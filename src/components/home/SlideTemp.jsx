b5486c9891f65ea05a33105547dfdb333c32eea1
rahibgasimli <rahib8425@gmail.com>
2024-10-26 12:03:48 +0400

    Ready

--- src/components/home/SlideTemp.jsx
+++ src/components/home/SlideTemp.jsx
@@ -5,7 +5,7 @@ import mainSlideImage from "../../assets/images/home/main-slide-image.svg"
 const SlideTemp = () => {
   return (
     <div className="home-main-slide">
-      <div className="main-slide-image"><img src={mainSlideImage} alt="" /></div>
+      <div className="main-slide-image"></div>
       <button>İndi al</button>
     </div>
   )
